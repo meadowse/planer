97681dab6489696ea9f56f5f1472253c3951561a
meadowse <baha.199620@gmail.com>
2026-06-16 12:16:52 +0300

    init django app movies

--- backPlanner/backPlanner/settings.py
+++ backPlanner/backPlanner/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # site packages
+    # my
+    "apiV1.apps.Apiv1Config",
 ]
 
 MIDDLEWARE = [
